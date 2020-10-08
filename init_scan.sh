8aa79aed8fce8411a415dbe071618d903d3a8a20
turkeyoak <jdwilson1@gmail.com>
2020-10-08 19:08:20 +0100

    Add check for reqs

--- init_scan.sh
+++ init_scan.sh
@@ -1,4 +1,18 @@
 echo "Quick hosts and services scan v.1"
+
+command_exists() {
+    # check if command exists and fail otherwise
+    command -v "$1" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        echo "I require $1 but it's not installed. Abort."
+        exit 1
+    fi
+}
+for COMMAND in "awk" "nmap"; do
+    command_exists "${COMMAND}"
+done
+
+
 ./findhosts.sh
 ./get_services.sh
 
